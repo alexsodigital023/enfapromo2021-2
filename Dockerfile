16361c422b1e9cee0ae01e864e784977fe5fad5e
paynalton <pay@soigital.mx>
2021-02-02 19:12:14 -0600

    production stage

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,20 @@
-FROM php:7.3-apache
+FROM php:7.4-apache
+
+RUN apt-get update && apt-get install -y git zip
+COPY ./src/api/config/vhost.conf /etc/apache2/sites-enabled/000-default.conf
+COPY ./src/api/src /var/www
+COPY ./src/api/config/global.php /var/www/config/autoload/global.php
+COPY ./src/api/config/IndexController.php /var/www/module/Application/src/Controller/IndexController.php
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN docker-php-ext-enable mysqli pdo pdo_mysql
+
+RUN curl -sS https://getcomposer.org/installer \
+  | php -- --install-dir=/usr/local/bin --filename=composer
+
+RUN a2enmod rewrite
+
+RUN mkdir -p  /var/www/data/cache && chmod 777 /var/www/data -R
+
+RUN cd /var/www && composer install --no-dev 
+COPY ./src/api/config/constants.php /var/www/src/ApiTools/constants.php
---
+++ src/api/config/global.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'api-tools-content-negotiation' => [
+        'selectors' => [],
+    ],
+    'db' => [
+        'adapters' => [
+            'database' => [
+                'driver' => \Mysqli::class,
+                'database' => 'enfa-goldenticket',
+                'username' => 'doadmin',
+                'password' => 'cperj0r7ng3hl9ld',
+                'hostname' => 'db-mysql-nyc1-42577-do-user-1684304-0.b.db.ondigitalocean.com',
+                'port' => '25060'
+            ],
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'oauth' => [
+                'options' => [
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ],
+                'type' => 'regex',
+            ],
+        ],
+    ],
+    'api-tools-mvc-auth' => [
+        'authentication' => [
+            'map' => [
+                'operator\\V1' => 'oauth',
+                'ping\\V1' => 'oauth',
+            ],
+        ],
+    ],
+    'storage' => [
+        'adapters' =>[
+            'do_spaces' =>[
+                'driver' => Aws\S3\S3Client::class,
+                'version' => 'latest',
+                'region'  => 'NYC3',
+                'endpoint' => 'https://nyc3.digitaloceanspaces.com',
+                'credentials' => [
+                        'key'    => 'M4I2LYAQVWAXFGF5W5XJ',
+                        'secret' => 'd+okr4VB3gZNPuwV0ubV34G6mxonCzbsTvxIRM3qSZk',
+                    ],
+                'bucket' => 'imatch'
+            ]
+        ]
+    ]
+];
