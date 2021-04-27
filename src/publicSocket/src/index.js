7dcd2f3c257d856e1a01239517134ddef83800a2
paynalton <pay@soigital.mx>
2021-04-27 13:49:48 -0500

    server

--- src/publicSocket/src/index.js
+++ src/publicSocket/src/index.js
@@ -1,5 +1,5 @@
 const websocket = require('ws');
-const https = require('http');
+const https = require('https');
 const fs = require('fs');
 const express = require('express');
 const handleLoader = require('./lib/LoadHandler');
