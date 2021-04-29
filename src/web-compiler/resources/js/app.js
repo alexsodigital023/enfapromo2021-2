b19589a354cf3a3e0633be3d9b5e74ea1be17597
paynalton <pay@soigital.mx>
2021-04-29 15:00:28 -0500

    arreglos wayin

--- src/web-compiler/resources/js/app.js
+++ src/web-compiler/resources/js/app.js
@@ -14,7 +14,7 @@ Vue.component('registros', require('./components/RegisterCounter.vue').default);
 Vue.component('rasca', require('./components/Rasca.vue').default);
 Vue.component('top-menu',require('./components/TopMenu').default);
 
-$(document).ready(()=>{
+
   (function ($) {
     const f = $("#xCampaignForm").get(0);
     const xF = $.parseHTML('<formulario ></formulario>');
@@ -25,9 +25,9 @@ $(document).ready(()=>{
     const xF1 = $.parseHTML('<top-menu ></top-menu>');
     $(xF1).insertBefore(f1);
     $(xF1).append(f1);
-    setTimeout(()=>{
+   /* setTimeout(()=>{
       NGX.entryForm.$el=$("#xCampaignForm").get(0)
-    },500);
+    },500);*/
   })($);
   
   const app = new Vue({
@@ -42,7 +42,7 @@ $(document).ready(()=>{
     mounted() {
     }
   });
-});
+
 
 
 
