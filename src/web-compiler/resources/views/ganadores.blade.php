1c42fe13a32be020fb64da8031ce78ec2e3efad2
paynalton <pay@soigital.mx>
2021-05-04 16:15:21 -0500

    ganadores

---
+++ src/web-compiler/resources/views/ganadores.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.Main',[
+    "menu"=>'index'
+    ])
+@section('title', '')
+@section('class', 'xSectionPrizes')
+@section('id', 'xSectionPrizes')
+@section('state', 'Prizes')
+@section('content')
+
+<div class="xSectionInner"><div id="xSectionInstructionsR1" class="xRow xRow-1col_full n1 odd first"><div class="xRowInner"><div id="xSectionInstructionsR1C1" class="xCell n1 first last full xModuleHeadline"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Headline" data-ngx-id="xInstructions_Headline" data-componenttype="headline" class="xComponent xHeadline n1 xFullColModule xStyle-default hasTitle xTmpl-Item" data-registered="true"><div class="xComponentInner"><div class="xItem xContentText xPostcard xClassHeadline xNetworkUgc xDisplayShow xVariantBottom hasCopy"><div class="xItemInner"><div class="xBodyContainer"><div class="xCopyContainer xMain xHoverHide"><h1 class="xTitle xHeading">Ganadores</h1></div></div></div></div></div></div></div></div></div></div> <div id="xSectionInstructionsR2" class="xRow xRow-1col_full n2 even"><div class="xRowInner"><div id="xSectionInstructionsR2C1" class="xCell n1 first last full xModuleHtml"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Content" data-ngx-id="xInstructions_Content" data-componenttype="html" class="xComponent xHtml n1 xFullColModule xTmpl-Default" data-registered="true"><div class="xComponentInner"><span class="here"></span></div></div></div></div></div></div></div>
+
+
+@endsection('content')
