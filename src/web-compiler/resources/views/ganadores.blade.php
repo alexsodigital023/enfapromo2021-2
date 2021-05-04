2a1b3736b5708f8c67d477a18c444156712490b3
Dafne Portillo <dafne@sodigital.mx>
2021-05-04 18:07:41 -0500

    Estilos de las instrucciones

--- src/web-compiler/resources/views/ganadores.blade.php
+++ src/web-compiler/resources/views/ganadores.blade.php
@@ -7,7 +7,13 @@
 @section('state', 'Prizes')
 @section('content')
 
-<div class="xSectionInner"><div id="xSectionInstructionsR1" class="xRow xRow-1col_full n1 odd first"><div class="xRowInner"><div id="xSectionInstructionsR1C1" class="xCell n1 first last full xModuleHeadline"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Headline" data-ngx-id="xInstructions_Headline" data-componenttype="headline" class="xComponent xHeadline n1 xFullColModule xStyle-default hasTitle xTmpl-Item" data-registered="true"><div class="xComponentInner"><div class="xItem xContentText xPostcard xClassHeadline xNetworkUgc xDisplayShow xVariantBottom hasCopy"><div class="xItemInner"><div class="xBodyContainer"><div class="xCopyContainer xMain xHoverHide"><h1 class="xTitle xHeading">Ganadores</h1></div></div></div></div></div></div></div></div></div></div> <div id="xSectionInstructionsR2" class="xRow xRow-1col_full n2 even"><div class="xRowInner"><div id="xSectionInstructionsR2C1" class="xCell n1 first last full xModuleHtml"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Content" data-ngx-id="xInstructions_Content" data-componenttype="html" class="xComponent xHtml n1 xFullColModule xTmpl-Default" data-registered="true"><div class="xComponentInner"><span class="here"></span></div></div></div></div></div></div></div>
+<div class="xSectionInner"><div id="xSectionInstructionsR1" class="xRow xRow-1col_full n1 odd first"><div class="xRowInner"><div id="xSectionInstructionsR1C1" class="xCell n1 first last full xModuleHeadline"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Headline" data-ngx-id="xInstructions_Headline" data-componenttype="headline" class="xComponent xHeadline n1 xFullColModule xStyle-default hasTitle xTmpl-Item" data-registered="true"><div class="xComponentInner"><div class="xItem xContentText xPostcard xClassHeadline xNetworkUgc xDisplayShow xVariantBottom hasCopy"><div class="xItemInner"><div class="xBodyContainer"><div class="xCopyContainer xMain xHoverHide"><h1 class="xTitle xHeading">Ganadores</h1></div></div></div></div></div></div></div></div></div></div> <div id="xSectionInstructionsR2" class="xRow xRow-1col_full n2 even"><div class="xRowInner"><div id="xSectionInstructionsR2C1" class="xCell n1 first last full xModuleHtml"><div class="xCellInner"><div id="xSectionInstructions_xInstructions_Content" data-ngx-id="xInstructions_Content" data-componenttype="html" class="xComponent xHtml n1 xFullColModule xTmpl-Default" data-registered="true"><div class="xComponentInner"><span class="here">
+<ol>
+    <li>COMPRA <span>$39.90 o más en productos Choco Milk&reg;</span></li>
+    <li>REGISTRA <span>tu tiket eb www.chocomilk.com.mx</span> </li>
+    <li>JUEGA Y GANA* <span>Podrás llevarte hasta $1000,000.00 M.N. y premios semanales</span></li>
+</ol>
+</span></div></div></div></div></div></div></div>
 
 
 @endsection('content')
