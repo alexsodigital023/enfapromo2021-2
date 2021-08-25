ea3b6be9210751e462be6fed745d58d12e3d9399
paynalton <pay@soigital.mx>
2021-08-25 09:42:11 -0500

    prod credentials

--- src/backend/config/services.php
+++ src/backend/config/services.php
@@ -34,7 +34,7 @@ return [
         'api' => env('GOOGLE_OCR_API', '')
     ],
     'cdp' =>[
-        'client_id'=>'md-so-digital-stage',
+        'client_id'=>'md-so-digital-prod',
         'client_secret'=>'R5k{8JX_$Zh',
         'basic_auth'=>'reckittbenckiser:t8uLkZMWNa87mxzKokKCXVQ9PEkwzXJv9YrHRD7r63WJwh5djf',
         'baseurl'=>'https://digital-security-authservice-production.frankfurt.rbdigitalcloud.com',
